0345fe05c3841ffb10e8df199cd3db8535f5ce9c
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-06-23 22:07:47 +0300

    main tag in HomePage Component for readability

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,10 +6,13 @@ const HomePage = () => {
   return (
     <>
       <Header />
-      <Timer />
-      <p>
-        Figma: <a href="https://www.figma.com/design/9ivq0ik74eDDixoEluNA7f/Untitled?node-id=0-1&p=f&t=TQhl8NFK8grWsRir-0">View Mobile and Desctop design</a>
-      </p>
+      <main>
+        <Timer />
+        <p>
+          Figma: <a href="https://www.figma.com/design/9ivq0ik74eDDixoEluNA7f/Untitled?node-id=0-1&p=f&t=TQhl8NFK8grWsRir-0">View Mobile and Desctop design</a>
+        </p>
+      </main>
+      
     </>
   )
 }
