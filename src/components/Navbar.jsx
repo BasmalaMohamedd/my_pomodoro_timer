4097b8599c968f69ac849f51aeefd0e2e72cd856
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-07-02 21:17:42 +0300

    navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import menuInOrange from "../assets/icons/menu-icon-inOrange.png"
+import menuInBeige from "../assets/icons/menu-icon-inBeige.png"
 const Navbar = () => {
+  //move state to the parent
+  const [isHomePage, setIsHomePage] = useState(true);
+  function toggle(){
+    setIsHomePage(prev=>!prev);
+    //open menu and close it based on isHomePage
+  }
   return (
-    <>
-      <h1>Navbar</h1>
-      <button>Menu</button>
-    </>
+    
+      <header>
+        <nav>
+          <button onClick={toggle} className='menu-icon-button'>
+            {
+              isHomePage? 
+              <img src={menuInOrange} alt="menu icon" className="menu-icon"/>:
+              <img src={menuInBeige} alt="menu icon" className="menu-icon"/>
+            }
+          </button>
+        </nav>
+      </header>
+    
   )
 }
 
